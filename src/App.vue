22f4ff7b6398fd0990d95600a99789a2728e89c2
Peter Chen <peter3598768@gmail.com>
2018-09-21 00:48:04 +0800

    initial project

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <img svg-inline src="./assets/img/normal.svg">
+    <normal />
+    <svg width="93px" height="48px" viewBox="0 0 93 48" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <!-- Generator: Sketch 51.1 (57501) - http://www.bohemiancoding.com/sketch -->
+        <title>btn/main/cancel/normal</title>
+        <desc>Created with Sketch.</desc>
+        <defs></defs>
+        <g id="btn/main/cancel/normal" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <rect id="Rectangle-3" fill-rule="nonzero" x="0" y="0" width="93" height="48"></rect>
+            <text id="CANCEL" font-family="Roboto" font-size="16" font-weight="bold" fill="#787878">
+                <tspan x="16.1679688" y="27">CANCEL</tspan>
+            </text>
+        </g>
+    </svg>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import normal from './assets/img/normal.svg'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    normal
+  }
+}
+</script>
+
+<style>
+
+@font-face {
+  font-family: 'Roboto';
+  src: url('./assets/fonts/Roboto-Regular.ttf');
+}
+
+#app {
+  font-family: 'Roboto';
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
